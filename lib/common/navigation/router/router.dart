42345c12ea24027506e1649a2061d8068cf6da29
@3beerJi <abeerjaydi@gmail.com>
2025-08-19 13:09:15 +0300

    edit activity

--- lib/common/navigation/router/router.dart
+++ lib/common/navigation/router/router.dart
@@ -1,11 +1,13 @@
 import 'package:amplify_trips_planner/common/navigation/router/routes.dart';
 import 'package:amplify_trips_planner/features/activity/ui/activity_page/activity_page.dart';
 import 'package:amplify_trips_planner/features/activity/ui/add_activity/add_activity_page.dart';
+import 'package:amplify_trips_planner/features/activity/ui/edit_activity/edit_activity_page.dart';
 import 'package:amplify_trips_planner/features/trip/ui/edit_trip_page/edit_trip_page.dart';
 import 'package:amplify_trips_planner/features/trip/ui/past_trip_page/past_trip_page.dart';
 import 'package:amplify_trips_planner/features/trip/ui/past_trips/past_trips_list.dart';
 import 'package:amplify_trips_planner/features/trip/ui/trip_page/trip_page.dart';
 import 'package:amplify_trips_planner/features/trip/ui/trips_list/trips_list_page.dart';
+import 'package:amplify_trips_planner/models/Activity.dart';
 import 'package:amplify_trips_planner/models/Trip.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -59,6 +61,13 @@ final router = GoRouter(
       builder: (context, state) {
         final activityId = state.pathParameters['id']!;
         return ActivityPage(activityId: activityId);
+      },
+    ),
+    GoRoute(
+      path: '/editactivity/:id',
+      name: AppRoute.editActivity.name,
+      builder: (context, state) {
+        return EditActivityPage(activity: state.extra! as Activity);
       },
     ),
   ],
